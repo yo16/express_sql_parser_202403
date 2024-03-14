b8e45177922c3d169c184a50e8403e2bcbaba332
yo16 <xxyo1xx@gmail.com>
2024-03-14 13:12:21 +0900

    add sql_parse_202403

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 const express = require("express");
+const Sql2Ary = require("sql_parse_202403");
+
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
+app.get("/sql", (req, res) => {
+  //const query1 = "select t1.col1 from t1 where t1.col2=\"abc\"";
+  const query2 = 
+  "with t2 as (" +
+      "select " +
+          "t1.id as id, name " +
+      "from t1" + 
+  ") select t2.col1 from t2, t3 where t2.id=t3.id";
+  const ary = Sql2Ary(query2);
+  
+  res.type('json').send({
+    ary
+  });
+});
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
