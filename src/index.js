c5a7c1b9f0e8da5e9f33131cb65689ae2dcf54b2
yo16 <xxyo1xx@gmail.com>
2024-03-19 00:08:20 +0900

    Sql2Aryの戻り値の変更対応

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Sql2Ary = require("sql_parse_202403");
+const { Sql2Ary } = require("sql_parse_202403");
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -15,15 +15,18 @@ app.get("/sql", (req, res) => {
 
   //const query1 = "select t1.col1 from t1 where t1.col2=\"abc\"";
   const query2 = 
-  "with t2 as (" +
-      "select " +
-          "t1.id as id, name " +
-      "from t1" + 
-  ") select t2.col1 from t2, t3 where t2.id=t3.id";
-  const ary = Sql2Ary(query2);
+    "with t2 as (" +
+    "select " +
+        "id, t1.name, col1 " +
+    "from t1" + 
+    ") select t2.col1 from t2, t3 where t2.id=t3.id";
+  const {stmts, tableConns, colConns} = Sql2Ary(query2);
+  //console.log(stmts);
   
   res.type('json').send({
-    statements: ary
+    statements: stmts,
+    tableConns,
+    colConns,
   });
 });
 
